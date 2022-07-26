bdba94942b400f80a4b2477de8407a5288b690a0
tanvirulislam <tanvirul.cse.diu@gmail.com>
2022-07-26 17:10:46 +0600

    search page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:amazon/screens/search_screen.dart';
+import 'package:amazon/utils/colors.dart';
 import 'package:amazon/widgets/search_bar.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -11,8 +12,50 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(child: SearchBar(), preferredSize: Size.fromHeight(80)),
-       
+        appBar: PreferredSize(
+          child: SearchBar(),
+          preferredSize: Size.fromHeight(80),
+        ),
+        body: Center(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 300,
+            color: Colors.amber,
+            height: 100,
+            child: InkWell(
+              onTap: () =>  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SearchScreen(),
+                  ),
+                ),
+              child: TextField(
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Search for something in Amazon',
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.mic_outlined),
+          )
+        ],
+      ),
+        ),
       ),
     );
   }
--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:amazon/screens/search_screen.dart';
 import 'package:amazon/utils/colors.dart';
 import 'package:amazon/utils/utils.dart';
 import 'package:flutter/material.dart';
@@ -10,38 +11,49 @@ class SearchBar extends StatelessWidget {
 
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    return Container(
-      height: 80,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: backgroundGradient),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          SizedBox(
-            width: screenSize.width * .7,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search for something in Amazon',
-                fillColor: Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SearchScreen(),
+          ),
+        );
+      },
+      child: Container(
+        height: 80,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: backgroundGradient),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: screenSize.width * .7,
+              child: Container(
+                height: 60,
+                width: screenSize.width * .7,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
                   borderRadius: BorderRadius.circular(7),
-                  borderSide: BorderSide(color: Colors.grey),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(7),
-                  borderSide: BorderSide(color: Colors.grey),
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Search for something in Amazon'),
                 ),
               ),
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.mic_outlined),
-          )
-        ],
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.mic_outlined),
+            )
+          ],
+        ),
       ),
     );
   }
